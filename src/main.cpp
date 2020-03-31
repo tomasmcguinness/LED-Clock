e8f7357555bfe5ae0d51694659472d259b8539c5
Tomas McGuinness <tomas@tomasmcguinness.com>
2020-03-31 14:50:41 +0100

    Added RTC Library and some code to integrate with the ticking clock

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,61 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <Wire.h> //I2C library
+#include <RtcDS3231.h> //RTC library
 
-#define NUM_LEDS 60
+#define NUM_LEDS 150
 CRGB leds[NUM_LEDS];
 
-void setup() 
-{ 
+RtcDS3231<TwoWire> rtcObject(Wire);
+
+void setup()
+{
   Serial.begin(9600);
-  FastLED.addLeds<NEOPIXEL, D2>(leds, NUM_LEDS); 
+  //FastLED.addLeds<NEOPIXEL, D2>(leds, NUM_LEDS);
+
+  rtcObject.Begin();  
+
+  RtcDateTime currentTime = RtcDateTime(16,05,18,21,20,0); //define date and time object
+  rtcObject.SetDateTime(currentTime);  
 }
 
-void loop() 
+void loop()
 {
-  Serial.write("On");
+  RtcDateTime currentTime = rtcObject.GetDateTime();    //get the time from the RTC
+ 
+  char str[20];   //declare a string as an array of chars
+ 
+  sprintf(str, "%d/%d/%d %d:%d:%d",     //%d allows to print an integer to the string
+          currentTime.Year(),   //get year method
+          currentTime.Month(),  //get month method
+          currentTime.Day(),    //get day method
+          currentTime.Hour(),   //get hour method
+          currentTime.Minute(), //get minute method
+          currentTime.Second()  //get second method
+         );
+ 
+  Serial.println(str);
 
-	leds[1] = CRGB::White; 
-  FastLED.show(); 
-  delay(30);
-	
-  Serial.write("Off");
+  // Serial.write("On");
 
-  leds[1] = CRGB::Black; 
-  FastLED.show(); 
-  delay(30);
+  // leds[1] = CRGB::White;
+  // FastLED.show();
+  // delay(30);
+
+  // Serial.write("Off");
+
+  // leds[1] = CRGB::Black;
+  // FastLED.show();
+  // delay(30);
+
+  // for (int dot = 0; dot < NUM_LEDS; dot++)
+  // {
+  //   leds[dot] = CRGB::Blue;
+  //   FastLED.show();
+  //   // clear this led for the next time around the loop
+  //   leds[dot] = CRGB::Black;
+  //   delay(6);
+  // }
+
+  delay(250);
 }
