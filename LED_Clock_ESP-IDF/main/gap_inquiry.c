d9f4b9240d57737175bd9dde34c4ccc752476499
Tomas McGuinness <tomas@tomasmcguinness.com>
2020-05-11 10:43:47 +0100

    Device now discoverable with correct EIR response.

--- LED_Clock_ESP-IDF/main/gap_inquiry.c
+++ LED_Clock_ESP-IDF/main/gap_inquiry.c
@@ -56,186 +56,66 @@
 #include <string.h>
 
 #include "btstack.h"
- 
-#define MAX_DEVICES 20
-enum DEVICE_STATE { REMOTE_NAME_REQUEST, REMOTE_NAME_INQUIRED, REMOTE_NAME_FETCHED };
-struct device {
-    bd_addr_t          address;
-    uint8_t            pageScanRepetitionMode;
-    uint16_t           clockOffset;
-    enum DEVICE_STATE  state; 
-};
 
-#define INQUIRY_INTERVAL 5
-struct device devices[MAX_DEVICES];
-int deviceCount = 0;
+static uint8_t eir_buffer[256];
 
+static const uint32_t GADGET_UUID_32 = 0xFE151510;
+static const uint16_t AMAZON_SIG_VID = 0x0171;
 
-enum STATE {INIT, ACTIVE} ;
-enum STATE state = INIT;
+static uint8_t gadget_uuid[16] = {0x60, 0x88, 0xd2, 0xb3, 0x98, 0x3a, 0x4e, 0xed, 0x9f, 0x94, 0x5a, 0xd1, 0x25, 0x68, 0x16, 0xb7};
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 
-static int getDeviceIndexForAddress( bd_addr_t addr){
-    int j;
-    for (j=0; j< deviceCount; j++){
-        if (bd_addr_cmp(addr, devices[j].address) == 0){
-            return j;
-        }
-    }
-    return -1;
-}
-
-static void start_scan(void){
-    printf("Starting inquiry scan..\n");
-    gap_inquiry_start(INQUIRY_INTERVAL);
-}
-
-static int has_more_remote_name_requests(void){
-    int i;
-    for (i=0;i<deviceCount;i++) {
-        if (devices[i].state == REMOTE_NAME_REQUEST) return 1;
-    }
-    return 0;
-}
-
-static void do_next_remote_name_request(void){
-    int i;
-    for (i=0;i<deviceCount;i++) {
-        // remote name request
-        if (devices[i].state == REMOTE_NAME_REQUEST){
-            devices[i].state = REMOTE_NAME_INQUIRED;
-            printf("Get remote name of %s...\n", bd_addr_to_str(devices[i].address));
-            gap_remote_name_request( devices[i].address, devices[i].pageScanRepetitionMode,  devices[i].clockOffset | 0x8000);
-            return;
-        }
-    }
-}
-
-static void continue_remote_names(void){
-    if (has_more_remote_name_requests()){
-        do_next_remote_name_request();
-        return;
-    } 
-    start_scan();
-}
-
-/* @section Bluetooth Logic 
- *
- * @text The Bluetooth logic is implemented as a state machine within the packet
- * handler. In this example, the following states are passed sequentially:
- * INIT, and ACTIVE.
- */ 
-
-static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
+{
     UNUSED(channel);
     UNUSED(size);
 
     bd_addr_t addr;
     int i;
     int index;
-    
-    if (packet_type != HCI_EVENT_PACKET) return;
+
+    if (packet_type != HCI_EVENT_PACKET)
+    {
+        return;
+    }
 
     uint8_t event = hci_event_packet_get_type(packet);
-
-    switch(state){ 
-        /* @text In INIT, an inquiry  scan is started, and the application transits to 
-         * ACTIVE state.
-         */
-        case INIT:
-            switch(event){
-                case BTSTACK_EVENT_STATE:
-                    if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING){
-                        start_scan();
-                        state = ACTIVE;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            break;
-
-        /* @text In ACTIVE, the following events are processed:
-         *  - GAP Inquiry result event: BTstack provides a unified inquiry result that contain
-         *    Class of Device (CoD), page scan mode, clock offset. RSSI and name (from EIR) are optional.
-         *  - Inquiry complete event: the remote name is requested for devices without a fetched 
-         *    name. The state of a remote name can be one of the following: 
-         *    REMOTE_NAME_REQUEST, REMOTE_NAME_INQUIRED, or REMOTE_NAME_FETCHED.
-         *  - Remote name request complete event: the remote name is stored in the table and the 
-         *    state is updated to REMOTE_NAME_FETCHED. The query of remote names is continued.
-         */
-        case ACTIVE:
-            switch(event){
-
-                case GAP_EVENT_INQUIRY_RESULT:
-                    if (deviceCount >= MAX_DEVICES) break;  // already full
-                    gap_event_inquiry_result_get_bd_addr(packet, addr);
-                    index = getDeviceIndexForAddress(addr);
-                    if (index >= 0) break;   // already in our list
-
-                    memcpy(devices[deviceCount].address, addr, 6);
-                    devices[deviceCount].pageScanRepetitionMode = gap_event_inquiry_result_get_page_scan_repetition_mode(packet);
-                    devices[deviceCount].clockOffset = gap_event_inquiry_result_get_clock_offset(packet);
-                    // print info
-                    printf("Device found: %s ",  bd_addr_to_str(addr));
-                    printf("with COD: 0x%06x, ", (unsigned int) gap_event_inquiry_result_get_class_of_device(packet));
-                    printf("pageScan %d, ",      devices[deviceCount].pageScanRepetitionMode);
-                    printf("clock offset 0x%04x",devices[deviceCount].clockOffset);
-                    if (gap_event_inquiry_result_get_rssi_available(packet)){
-                        printf(", rssi %d dBm", (int8_t) gap_event_inquiry_result_get_rssi(packet));
-                    }
-                    if (gap_event_inquiry_result_get_name_available(packet)){
-                        char name_buffer[240];
-                        int name_len = gap_event_inquiry_result_get_name_len(packet);
-                        memcpy(name_buffer, gap_event_inquiry_result_get_name(packet), name_len);
-                        name_buffer[name_len] = 0;
-                        printf(", name '%s'", name_buffer);
-                        devices[deviceCount].state = REMOTE_NAME_FETCHED;;
-                    } else {
-                        devices[deviceCount].state = REMOTE_NAME_REQUEST;
-                    }
-                    printf("\n");
-                    deviceCount++;
-                    break;
-
-                case GAP_EVENT_INQUIRY_COMPLETE:
-                    for (i=0;i<deviceCount;i++) {
-                        // retry remote name request
-                        if (devices[i].state == REMOTE_NAME_INQUIRED)
-                            devices[i].state = REMOTE_NAME_REQUEST;
-                    }
-                    continue_remote_names();
-                    break;
-
-                case HCI_EVENT_REMOTE_NAME_REQUEST_COMPLETE:
-                    reverse_bd_addr(&packet[3], addr);
-                    index = getDeviceIndexForAddress(addr);
-                    if (index >= 0) {
-                        if (packet[2] == 0) {
-                            printf("Name: '%s'\n", &packet[9]);
-                            devices[index].state = REMOTE_NAME_FETCHED;
-                        } else {
-                            printf("Failed to get name: page timeout\n");
-                        }
-                    }
-                    continue_remote_names();
-                    break;
-
-                default:
-                    break;
-            }
-            break;
-            
-        default:
-            break;
-    }
 }
 
-/* @text For more details on discovering remote devices, please see
- * Section on [GAP](../profiles/#sec:GAPdiscoverRemoteDevices).
- */
+void build_eir(uint8_t *buffer, const char *name, uint16_t pid, uint16_t vid)
+{
+    uint8_t *ptr = buffer;
 
+    // 0x09 Complete Local Name
+    uint8_t name_len = strlen(name);
+    *ptr++ = name_len + 1;
+    *ptr++ = BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME;
+    memcpy(ptr, name, name_len);
+    ptr += name_len;
+
+    // 0x06 Incomplete List of 128-bit Service Class UUIDs
+    *ptr++ = 1 + 16; // 1 byte for DataType + 128 bit per each uuid
+    *ptr++ = BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_128_BIT_SERVICE_CLASS_UUIDS;
+    for (int8_t i = 15; i >= 0; --i)
+    {
+        *ptr++ = gadget_uuid[i];
+    }
+
+    // 0xFF Manufacturer Specific Data
+    *ptr++ = 1 + 10; // 1 byte for DataType + 10 for custom data
+    *ptr++ = BLUETOOTH_DATA_TYPE_MANUFACTURER_SPECIFIC_DATA;
+    *ptr++ = vid & 0xff;
+    *ptr++ = (vid >> 8) & 0xff;
+    *ptr++ = pid & 0xff;
+    *ptr++ = (pid >> 8) & 0xff;
+    *ptr++ = AMAZON_SIG_VID & 0xff;
+    *ptr++ = AMAZON_SIG_VID >> 8;
+    *ptr++ = GADGET_UUID_32 & 0xff;
+    *ptr++ = (GADGET_UUID_32 >> 8) & 0xff;
+    *ptr++ = (GADGET_UUID_32 >> 16) & 0xff;
+    *ptr++ = (GADGET_UUID_32 >> 24) & 0xff;
+}
 
 /* @section Main Application Setup
  *
@@ -244,20 +124,30 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
  */
 
 /* LISTING_START(MainConfiguration): Setup packet handler for GAP inquiry */
-int btstack_main(int argc, const char * argv[]);
-int btstack_main(int argc, const char * argv[]) {
+int btstack_main(int argc, const char *argv[]);
+int btstack_main(int argc, const char *argv[])
+{
     (void)argc;
     (void)argv;
 
     // enabled EIR
     hci_set_inquiry_mode(INQUIRY_MODE_RSSI_AND_EIR);
 
+    // build EIR for Alexa Gadget
+    memset(eir_buffer, 0, sizeof(eir_buffer));
+    build_eir(eir_buffer, "LED CLOCK", 0x0000, 0xffff);
+    gap_set_extended_inquiry_response(eir_buffer);
+
+    // get the HCI callback handler
     hci_event_callback_registration.callback = &packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
 
+    // make this device discoverable
+    gap_discoverable_control(1);
+
     // turn on!
     hci_power_control(HCI_POWER_ON);
-        
+
     return 0;
 }
 /* LISTING_END */
