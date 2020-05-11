24df5ef40f697a8760a0d344f7d2c8cb604b7919
Tomas McGuinness <tomas@tomasmcguinness.com>
2020-05-11 12:06:26 +0100

    Working up to responding to alexa discover event!!!!

--- LED_Clock_ESP-IDF/main/gap_inquiry.c
+++ LED_Clock_ESP-IDF/main/gap_inquiry.c
@@ -13,26 +13,271 @@
 
 #define TAG "LEDCLOCK"
 
+static bd_addr_t alexa_bd_addr = {0x44, 0x00, 0x49, 0x0F, 0x6A, 0x10};
+
 static uint8_t eir_buffer[256];
 static uint8_t sdp_buffer_ota[512];
 static uint8_t sdp_buffer_data[512];
 static uint8_t sdp_buffer_gadget[512];
 
+static uint8_t comm_buffer[1 << 13];
+static uint16_t comm_buffer_len;
+static bool inside_frame;
+static bool escape;
+static bool frame_parsing_error;
+
+uint16_t rfcomm_cid;
+
+uint8_t outgoing_buffer[1 << 13];
+uint16_t outgoing_buffer_len;
+uint8_t seq_id;
+
 static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static void handle_sdp_client_query_result(uint8_t packet_type,
+                                           uint16_t channel, uint8_t *packet,
+                                           uint16_t size)
+{
+    switch (packet[0])
+    {
+    case SDP_EVENT_QUERY_ATTRIBUTE_VALUE:
+        // TODO: anything useful here?
+        break;
+
+    case SDP_EVENT_QUERY_COMPLETE:
+        if (sdp_event_query_complete_get_status(packet))
+        {
+            ESP_LOGI(TAG, "SDP query failed 0x%02x", sdp_event_query_complete_get_status(packet));
+            break;
+        }
+        ESP_LOGI(TAG, "SDP query done.");
+        break;
+    }
+}
+
+void reset_comm_packet()
+{
+    memset(comm_buffer, 0, sizeof(comm_buffer));
+    comm_buffer_len = 0;
+    inside_frame = false;
+    escape = false;
+    frame_parsing_error = false;
+}
+
+uint8_t get_next_sequence_id()
+{
+    do
+    {
+        ++seq_id;
+    } while (seq_id == START_OF_FRAME || seq_id == END_OF_FRAME || seq_id == ESCAPE);
+
+    return seq_id;
+}
+
+uint16_t get_packet_checksum(uint8_t *buffer, uint16_t len)
+{
+    if (buffer[0] != START_OF_FRAME)
+    {
+        ESP_LOGE(TAG, "Invalid packet, wrong START_OF_FRAME byte");
+    }
+    if (buffer[len - 1] != END_OF_FRAME)
+    {
+        ESP_LOGE(TAG, "Invalid packet, wrong END_OF_FRAME byte");
+    }
+    // TODO: do we need to handle escapes here?
+    uint16_t checksum = buffer[1] + buffer[2];
+    uint16_t index;
+    bool escape = false;
+    for (index = 4; index + 3 < len; ++index)
+    {
+        if (buffer[index] == ESCAPE)
+        {
+            escape = true;
+            continue;
+        }
+        if (escape)
+        {
+            checksum += buffer[index] ^ ESCAPE;
+            escape = false;
+        }
+        else
+        {
+            checksum += buffer[index];
+        }
+    }
+    return checksum;
+}
+
+void handle_alexa_packet()
+{
+    uint16_t checksum = get_packet_checksum(comm_buffer, comm_buffer_len);
+    ESP_LOGI(TAG, "Alexa Gadgets Toolkit packet, checksum: %02x %02x", (checksum >> 8) & 0xff, checksum & 0xff);
+    // get_next_sequence_id(); // TODO: remove?
+    /* log_info_hexdump(comm_buffer, comm_buffer_len); */
+    // TODO: add checks
+    handle_alexa_payload(comm_buffer + 4, comm_buffer_len - 7);
+}
+
+void handle_alexa_rfcomm_packet(uint8_t *packet, uint16_t size)
+{
+    for (uint16_t i = 0; i < size; ++i)
+    {
+        if (comm_buffer_len >= sizeof(comm_buffer))
+        {
+            frame_parsing_error = true;
+        }
+
+        if (packet[i] == START_OF_FRAME)
+        {
+            reset_comm_packet();
+        }
+
+        if (frame_parsing_error)
+        {
+            continue;
+        }
+
+        switch (packet[i])
+        {
+        case START_OF_FRAME:
+            if (inside_frame)
+            {
+                // END_OF_FRAME was expected first
+                frame_parsing_error = true;
+            }
+            else
+            {
+                comm_buffer[comm_buffer_len++] = packet[i];
+                inside_frame = true;
+            }
+            break;
+
+        case END_OF_FRAME:
+            if (!inside_frame)
+            {
+                // START_OF_FRAME was expected first
+                frame_parsing_error = true;
+            }
+            else
+            {
+                comm_buffer[comm_buffer_len++] = packet[i];
+                inside_frame = false;
+                handle_alexa_packet();
+            }
+            break;
+
+        case ESCAPE:
+            escape = true;
+            break;
+
+        default:
+            if (!inside_frame)
+            {
+                frame_parsing_error = true;
+            }
+            else
+            {
+                if (escape)
+                {
+                    comm_buffer[comm_buffer_len++] = packet[i] ^ ESCAPE;
+                    escape = false;
+                }
+                else
+                {
+                    comm_buffer[comm_buffer_len++] = packet[i];
+                }
+            }
+            break;
+        }
+    }
+}
+
+void outgoing_data_is_ready(uint8_t *buffer, uint16_t len)
+{
+    ESP_LOGI(TAG, "rfcomm_request_can_send_now_event");
+    // TODO: find a better place for this wrapper into SPP packet
+    outgoing_buffer_len = 0;
+    outgoing_buffer[outgoing_buffer_len++] = START_OF_FRAME;
+    outgoing_buffer[outgoing_buffer_len++] = PACKET_ID;
+    outgoing_buffer[outgoing_buffer_len++] = ERROR_ID;
+    outgoing_buffer[outgoing_buffer_len++] = get_next_sequence_id();
+
+    uint16_t index;
+    for (index = 0; index < len; ++index)
+    {
+        if (buffer[index] == START_OF_FRAME || buffer[index] == END_OF_FRAME ||
+            buffer[index] == ESCAPE)
+        {
+            outgoing_buffer[outgoing_buffer_len++] = ESCAPE;
+            outgoing_buffer[outgoing_buffer_len++] = buffer[index] ^ ESCAPE;
+        }
+        else
+        {
+            outgoing_buffer[outgoing_buffer_len++] = buffer[index];
+        }
+    }
+
+    outgoing_buffer[outgoing_buffer_len++] = 0;
+    outgoing_buffer[outgoing_buffer_len++] = 0;
+    outgoing_buffer[outgoing_buffer_len++] = END_OF_FRAME;
+
+    // TODO: cleaner way to write checksum
+    uint16_t checksum = get_packet_checksum(outgoing_buffer, outgoing_buffer_len);
+    outgoing_buffer[outgoing_buffer_len - 3] = (checksum >> 8) & 0xff;
+    outgoing_buffer[outgoing_buffer_len - 2] = checksum & 0xff;
+    rfcomm_request_can_send_now_event(rfcomm_cid);
+}
+
 
 static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
 {
     UNUSED(channel);
     UNUSED(size);
 
+    bd_addr_t bd_addr;
+
     switch (packet_type)
     {
     case HCI_EVENT_PACKET:
         ESP_LOGI(TAG, "Got HCI packet");
+
+        switch (hci_event_packet_get_type(packet))
+        {
+        case BTSTACK_EVENT_STATE:
+            // BTstack activated, get started
+            // TODO: get Alexa address from NVM storage
+            if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING)
+            {
+                sdp_client_query_uuid16(&handle_sdp_client_query_result,
+                                        alexa_bd_addr,
+                                        BLUETOOTH_ATTRIBUTE_PUBLIC_BROWSE_ROOT);
+            }
+            break;
+        case RFCOMM_EVENT_CHANNEL_OPENED:
+            break;
+        case RFCOMM_EVENT_CHANNEL_CLOSED:
+            break;
+        case RFCOMM_EVENT_CAN_SEND_NOW:
+            ESP_LOGI(TAG, "RFCOMM_EVENT_CAN_SEND_NOW");
+            rfcomm_send(rfcomm_cid, outgoing_buffer, outgoing_buffer_len);
+            memset(outgoing_buffer, 0, sizeof(outgoing_buffer));
+            outgoing_buffer_len = 0;
+            seq_id = 0;
+            break;
+        case RFCOMM_EVENT_INCOMING_CONNECTION:
+            rfcomm_event_incoming_connection_get_bd_addr(packet, bd_addr);
+            rfcomm_cid = rfcomm_event_incoming_connection_get_rfcomm_cid(packet);
+            ESP_LOGI(TAG, "Accepting RFCOMM connection from %s, cid: %d",
+                     bd_addr_to_str(bd_addr), rfcomm_cid);
+            rfcomm_accept_connection(rfcomm_cid);
+            break;
+        }
+        break;
         break;
 
     case RFCOMM_DATA_PACKET:
         ESP_LOGI(TAG, "Got RFCOMM data on channel %d", channel);
+        handle_alexa_rfcomm_packet(packet, size);
         break;
 
     default:
@@ -46,7 +291,7 @@ int btstack_main(int argc, const char *argv[])
     (void)argv;
 
     // enabled EIR
-    hci_set_inquiry_mode(INQUIRY_MODE_RSSI_AND_EIR);
+    //hci_set_inquiry_mode(INQUIRY_MODE_RSSI_AND_EIR);
 
     // build EIR for Alexa Gadget
     memset(eir_buffer, 0, sizeof(eir_buffer));
