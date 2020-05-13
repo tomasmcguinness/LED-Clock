892bd79e0d9b61ecb6470b62e5eeeafbd22213a9
Tomas McGuinness <tomas@tomasmcguinness.com>
2020-05-13 09:04:25 +0100

    Changed strip type. Colours are still odd, but booting loop is now doing something.

--- LED_Clock_ESP-IDF/main/clock.cpp
+++ LED_Clock_ESP-IDF/main/clock.cpp
@@ -2,7 +2,7 @@
 
 #define NUM_LEDS 144
 #define DATA_PIN 16
-#define LED_TYPE WS2812
+#define LED_TYPE NEOPIXEL
 
 static const char *TAG = "clock.cpp";
 
@@ -12,8 +12,9 @@ int i = 0;
 
 // https://github.com/turiphro/alexa-led-ring-animations/blob/master/Alexa_Ring_Animations.ino
 // Alexa colours
-const uint32_t BOOTING_BG = CRGB(0, 0, 255);
-const uint32_t BOOTING_FG = CRGB(0, 255, 255);
+//
+const uint32_t BOOTING_BG = CRGB::Blue;
+const uint32_t BOOTING_FG = CRGB::DarkBlue;
 
 void fill(uint32_t colour)
 {
@@ -23,14 +24,13 @@ void fill(uint32_t colour)
     }
 }
 
-void spinner(uint32_t colour_bg, uint32_t colour_fg, int pos, int width = ceil(NUM_LEDS / 12.0));
 void spinner(uint32_t colour_bg, uint32_t colour_fg, int pos, int width)
 {
     fill(colour_bg);
 
     for (int i = pos; i < pos + width; i++)
     {
-        leds[i % NUM_LEDS] = colour_fg; // rotate clockwise
+        leds[i % NUM_LEDS] = colour_fg;
     }
 }
 
@@ -40,18 +40,54 @@ unsigned long last_increment = millis();
 const int DURATION = 1000; // animation duration (ms)
 const int STEP_SIZE = DURATION / NUM_LEDS;
 
-enum mode {
-  BOOTING,
-  CLOCK,
-  TIMER,
-  MESSAGE
+enum mode
+{
+    BOOTING,
+    CLOCK,
+    TIMER,
+    MESSAGE
 } mode;
+
+#define SEC_PER_DAY 86400
+#define SEC_PER_HOUR 3600
+#define SEC_PER_MIN 60
+
+void move_hands()
+{
+    // struct timeval tv;
+    // struct timezone tz;
+    // gettimeofday(&tv, &tz);
+
+    // long hms = tv.tv_sec % SEC_PER_DAY;
+    // hms += tz.tz_dsttime * SEC_PER_HOUR;
+    // hms -= tz.tz_minuteswest * SEC_PER_MIN;
+    // // mod `hms` to insure in positive range of [0...SEC_PER_DAY)
+    // hms = (hms + SEC_PER_DAY) % SEC_PER_DAY;
+
+    // // Tear apart hms into h:m:s
+    // int hour = hms / SEC_PER_HOUR;
+    // int min = (hms % SEC_PER_HOUR) / SEC_PER_MIN;
+    // int sec = (hms % SEC_PER_HOUR) % SEC_PER_MIN; // or hms % SEC_PER_MIN
+
+    // ESP_LOGI(TAG, "Current local time: %d:%02d:%02d\n", hour, min, sec);
+}
 
 void animate_clock(void *pvParameters)
 {
     while (true)
     {
-        spinner(BOOTING_BG, BOOTING_FG, counter);
+        switch (mode)
+        {
+        case BOOTING:
+            spinner(BOOTING_BG, BOOTING_FG, counter, 12);
+            break;
+        case CLOCK:
+            move_hands();
+            break;
+        default:
+            break;
+        }
+
         FastLED.show();
 
         if (millis() - last_increment > STEP_SIZE || millis() < last_increment)
@@ -62,7 +98,6 @@ void animate_clock(void *pvParameters)
 
             counter = nextCount % NUM_LEDS;
 
-            //counter = (++counter % NUM_LEDS);
             if (counter == 0)
             {
                 //up = !up; // swap
@@ -89,6 +124,5 @@ extern "C" void setup_clock()
 extern "C" void set_time()
 {
     ESP_LOGI(TAG, "Time has been set");
-
     mode = CLOCK;
 }
